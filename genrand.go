c6f8528cf238c7e2e3d528c7fed7c497342ad936
Bruce Ediger <bediger8@gmail.com>
2020-05-04 15:41:46 -0600

    add -p XXX for 3rd population column of data

--- genrand.go
+++ genrand.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"math/rand"
@@ -10,14 +11,27 @@ import (
 )
 
 func main() {
-	N, err := strconv.Atoi(os.Args[1])
+	maxPop := flag.Int("p", 0, "maxium population")
+	flag.Parse()
+
+	args := flag.Args()
+
+	N, err := strconv.Atoi(args[0])
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	population := *maxPop > 0
+
 	rand.Seed(time.Now().UnixNano() + int64(os.Getpid()))
 
-	for i := 0; i < N; i++ {
-		fmt.Printf("%f %f\n", 100.*rand.Float64(), 100.0*rand.Float64())
+	if population {
+		for i := 0; i < N; i++ {
+			fmt.Printf("%d %f %f\n", rand.Intn(*maxPop), 1500.*rand.Float64(), 1500.0*rand.Float64())
+		}
+	} else {
+		for i := 0; i < N; i++ {
+			fmt.Printf("%f %f\n", 1500.*rand.Float64(), 1500.0*rand.Float64())
+		}
 	}
 }
