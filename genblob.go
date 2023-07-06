492df03e5d92121199576e8a07d6f8c16a08fa48
Bruce Ediger <bediger8@gmail.com>
2023-07-05 21:32:17 -0600

    comment so as to understand later

--- genblob.go
+++ genblob.go
@@ -1,5 +1,12 @@
 package main
 
+/*
+ * genblob - write text coordinates of circular "blobs"
+ * or clusters of points on stdout.
+ *
+ * Usage: genblob $clusters $total_points
+ */
+
 import (
 	"fmt"
 	"log"
@@ -11,18 +18,30 @@ import (
 )
 
 func main() {
-	N, err := strconv.Atoi(os.Args[1])
+	max, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	N, err := strconv.Atoi(os.Args[2])
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	rand.Seed(time.Now().UnixNano() + int64(os.Getpid()))
 
-	for i := 0; i < 2; i++ {
+	// max is number of "blobs" or clusters
+	for i := 0; i < max; i++ {
 		x0 := 100. * rand.Float64()
 		y0 := 100. * rand.Float64()
+		fmt.Fprintf(os.Stderr, "# Blob %d X0,Y0 %f,%f\n", i, x0, y0)
 
-		for i := 0; i < N/2; i++ {
+		// N/max count of points per blob.
+		// This will produce point-density higher the closter
+		// to <x0,y0> you get, since a given "racetrack"
+		// has more area as radius increases, but the uniform
+		// distribution of rand.Float64 will put the same number
+		// of points on any given radius.
+		for i := 0; i < N/max; i++ {
 			th := 6.28 * rand.Float64()
 			r := 50. * rand.Float64()
 			x := x0 + r*math.Cos(th)
